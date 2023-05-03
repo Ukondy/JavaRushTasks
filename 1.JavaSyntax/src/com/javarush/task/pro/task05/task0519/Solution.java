9823580e682411dd2e8aca67c4da1774bec79fcb
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:45:41 +0300

    add JavaRushTasks JavaSyntax pro task 0519

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task05.task0519;
+
+import java.util.Arrays;
+
+/* 
+Есть ли кто?
+*/
+
+public class Solution {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 5;
+
+    public static void main(String[] args) {
+        int []copyArray = Arrays.copyOf(array, array.length);
+        Arrays.sort(copyArray);
+        int el = Arrays.binarySearch(copyArray, element);
+        System.out.println(el >= 0 ? "true" : "false");
+    }
+}
