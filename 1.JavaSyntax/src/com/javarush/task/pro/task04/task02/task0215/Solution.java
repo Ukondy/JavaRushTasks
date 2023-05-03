583d015ea33f427a636f4829049d16465c5aaac7
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:31:46 +0300

    add JavaRushTasks JavaSyntax pro task 0215

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task02/task0215/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task04.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        int sc1 = sc.nextInt();
+        int sc2 = sc.nextInt();
+        int sc3 = sc.nextInt();
+        System.out.println((sc1 + sc2 + sc3) / 3);
+    }
+}
