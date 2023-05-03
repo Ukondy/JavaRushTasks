40588c1c0ec6a063bca1657f9a4c8172539854c0
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:17:56 +0300

    add JavaRushTasks JavaSyntax task 0142

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0142;
+
+/* 
+Амиго очень умный
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Амиго очень умный");
+    }
+}
