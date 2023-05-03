b933f5ea98701c23cb5912d81b5b38ebc261b471
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:31:23 +0300

    add JavaRushTasks JavaSyntax pro task 0214

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task02/task0214/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task04.task02.task0214;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/* 
+Чтение и преобразование строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String sc1 = sc.nextLine();
+        String sc2 = sc.nextLine();
+        String sc3 = sc.nextLine();
+        System.out.println(sc3);
+        System.out.println(sc2.toUpperCase());
+        System.out.println(sc1.toLowerCase());
+
+    }
+}
