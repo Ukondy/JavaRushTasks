063fc54c475aa5c11dfa8427fdebf1689e8230f5
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:26:04 +0300

    add JavaRushTasks JavaSyntax pro task 0202

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task02/task0202/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task04.task02.task0202;
+
+/* 
+Полнометражная картина
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int centimeters = 243;
+
+        int meters = centimeters / 100; //напишите тут ваш код
+
+        System.out.println(meters);
+    }
+}
