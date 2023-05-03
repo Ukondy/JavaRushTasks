eae87431edb487979dd72a712f231cb5a3f0d871
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:38:46 +0300

    add JavaRushTasks JavaSyntax pro task 0418

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0418/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0418;
+
+import java.util.Scanner;
+
+/* 
+Стакан наполовину пуст или наполовину полон?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        double glass = 0.5;
+        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        if(sc.nextBoolean()) {
+            System.out.println((int)Math.ceil(glass));
+        } else {
+            System.out.println((int)Math.floor(glass));
+        }
+    }
+}
