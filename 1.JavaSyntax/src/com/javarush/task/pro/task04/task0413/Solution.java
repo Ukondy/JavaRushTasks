bc062ce1bcb30aad0615514987f42827eca14c02
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:37:06 +0300

    add JavaRushTasks JavaSyntax pro task 0413

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0413;
+
+/* 
+Рисуем треугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("8");
+            }
+            System.out.println("8");
+        }
+    }
+}
