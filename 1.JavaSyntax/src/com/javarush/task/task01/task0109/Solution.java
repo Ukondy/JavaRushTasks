0022897687231cbed679506b168b66a2dad965e1
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:16:49 +0300

    add JavaRushTasks JavaSyntax task 0109

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+    }
+}
