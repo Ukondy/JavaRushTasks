2dfabfbfc5452478ae4a398ed0b700ac1d3dd2a9
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:28:22 +0300

    add JavaRushTasks JavaSyntax pro task 0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task02/task0206/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task04.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String word = "21";
+        String phrase = "dwa";
+        String line = "dww";
+        String text = "wq";
+    }
+}
