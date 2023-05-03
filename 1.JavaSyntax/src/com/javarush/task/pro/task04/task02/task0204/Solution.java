0b1abb9b235f4852db6776c1cff33814c4bb9b70
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:27:28 +0300

    add JavaRushTasks JavaSyntax pro task 0204

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task02/task0204/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task02.task0204;
+
+/* 
+9-ка это перевернутая 6-ка
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int six = 6;
+        six++;
+        six++;
+        six++;
+
+        //напишите тут ваш код
+
+
+        System.out.println(six);
+    }
+}
