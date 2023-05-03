2485df986e6d446959d202da6c3485b009340250
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:39:54 +0300

    add JavaRushTasks JavaSyntax pro task 0501

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0501/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task05.task0501;
+
+/* 
+Создаем массивы
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int []intArray = new int[10];
+        double []doubleArray = new double[10];
+        
+    }
+}
