bb13c45470c3ae8af6d9d32f7d34addf1689e583
ваше имяДима <dimapotrebic@gmail.com>
2023-05-03 22:32:13 +0300

    add JavaRushTasks JavaSyntax pro task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        //напишите тут ваш код
+        int i = 0;
+        while (i != 100) {
+            i++;
+            System.out.println(quote);
+        }
+    }
+}
