c2eea76ab620bb5747f18c77fe1310480661cc15
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:44:46 +0300

    add JavaRushTasks JavaSyntax pro task 0610

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0610/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task06.task0610;
+
+/* 
+Борьба за доступ
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Person person = new Person("Иван", "Иванов");
+        person.getFullName().equals(person.getFirstName() + person.getLastName());
+        System.out.println("Досье.");
+        System.out.println("Имя: " + person.getFirstName());
+        System.out.println("Фамилия: " + person.getLastName());
+        System.out.println("Полное имя: " + person.getFullName());
+    }
+}
