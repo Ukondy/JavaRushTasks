f28418291164d700223b743c6fef4f4115ee9d03
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:34:40 +0300

    add JavaRushTasks JavaSyntax pro task 1509

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1509/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task15.task1509;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Scanner;
+
+/* 
+Еще раз читаем из консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String line = scanner.nextLine();
+            char[] chars = line.toCharArray();
+            Set<Character> characters = new HashSet<>();
+            for (char aChar : chars) {
+                if (Character.isAlphabetic(aChar)) {
+                    characters.add(Character.toLowerCase(aChar));
+                }
+            }
+            System.out.println(characters);
+        }
+    }
+}
+
