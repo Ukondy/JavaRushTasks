c8305db51de1cd4e991d41d317ff6d4cef3a63bf
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:51:26 +0300

    add JavaRushTasks JavaSyntax pro task 1824

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1824/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task18.task1824;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+/* 
+Из потока данных — в map
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("JavaRush", "CodeGym", "Amigo", "Elly", "Kim", "Risha");
+
+        getMap(stringStream).forEach((s, i) -> System.out.println(s + " - " + i));
+    }
+
+    public static Map<String, Integer> getMap(Stream<String> stringStream) {
+        Map<String, Integer> map = stringStream
+                .collect(Collectors.toMap(e -> e, e -> e.length()));
+        return map;
+    }
+}
