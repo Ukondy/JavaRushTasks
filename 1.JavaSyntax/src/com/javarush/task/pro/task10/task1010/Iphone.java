e7e69c9dff1032e9129b586f21cecec51b49c770
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 22:08:46 +0300

    add JavaRushTasks JavaSyntax pro task 1010

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1010/Iphone.java
@@ -0,0 +1,42 @@
+package com.javarush.task.pro.task10.task1010;
+
+import java.util.Objects;
+
+/* 
+Два айфона
+*/
+
+public class Iphone {
+    private String model;
+    private String color;
+    private int price;
+
+    public Iphone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Iphone iphone = (Iphone) o;
+        return price == iphone.price &&
+                Objects.equals(model, iphone.model) &&
+                Objects.equals(color, iphone.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, color, price);
+    }
+
+    public static void main(String[] args) {
+        Iphone iphone1 = new Iphone("X", "Black", 999);
+        Iphone iphone2 = new Iphone("X", "Black", 999);
+
+        System.out.println(iphone1.equals(iphone2));
+    }
+
+}
