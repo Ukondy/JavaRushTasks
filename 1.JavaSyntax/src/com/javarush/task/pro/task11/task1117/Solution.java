226e81b05842292457f587f54cd840c115f96b66
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 22:18:26 +0300

    add JavaRushTasks JavaSyntax pro task 1117

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1117/Solution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task11.task1117;/*
+Пакет нужен?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Test package");
+    }
+}
