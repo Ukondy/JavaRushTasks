dd2c79007323ce07f586804f6e53ef57218b984a
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 22:04:38 +0300

    add JavaRushTasks JavaSyntax pro task 0914

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0914/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task09.task0914;
+
+/* 
+Обновление пути
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin/";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        int index = path.indexOf("jdk");
+        String jdk1 = path.substring(index, path.indexOf("/", index));
+        return path.replaceAll(jdk1, jdk);
+    }
+}
