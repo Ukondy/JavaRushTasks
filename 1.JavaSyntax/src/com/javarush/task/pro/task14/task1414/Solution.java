9a8641f9c19b495cd80978129e2738d07959f2e8
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:31:46 +0300

    add JavaRushTasks JavaSyntax pro task 1414

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1414/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task14.task1414;
+
+/* 
+Готовим коктейли
+*/
+
+public class Solution {
+
+    public static final String OUTPUT_FORMAT = "Метод %s вызван из строки %d класса %s в файле %s.\n";
+
+    public static void main(String[] args) {
+        makeScrewdriver();
+    }
+
+    public static void printStackTrace(StackTraceElement[] stackTrace) {
+        for (StackTraceElement element : stackTrace) {
+            System.out.printf(OUTPUT_FORMAT, element.getMethodName(), element.getLineNumber(), element.getClassName(), element.getFileName());
+        }
+    }
+
+    static void makeScrewdriver() {
+        addJuice();
+    }
+
+    static void addJuice() {
+        addVodka();
+    }
+
+    static void addVodka() {
+        printStackTrace(Thread.currentThread().getStackTrace());
+    }
+}
