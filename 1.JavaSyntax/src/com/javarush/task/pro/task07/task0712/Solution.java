c3be8f1aae64ed0da8e12c924befbba734c44a5a
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:52:16 +0300

    add JavaRushTasks JavaSyntax pro task 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String a = new String();
+        String b = new String();
+        String c = new String();
+        String d = new String();
+        String e = new String();
+        String f = new String();
+        String g = new String();
+        String h = new String();
+        String i = new String();
+        String j = new String();
+
+        int z[] = new int [1];
+        int x[] = new int [2];
+        int n[] = new int [3];
+        int m[] = new int [4];
+        int l[] = new int [5];
+
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+    }
+}
