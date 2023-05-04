df2a68c336141964de3aeb8ddabe8919d20dea9d
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:23:27 +0300

    add JavaRushTasks JavaSyntax pro task 1301

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1301/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task13.task1301;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+/* 
+Оформляем возврат
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] array = {"Через", "три", "года", "я", "буду", "Senior", "Java", "Developer"};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("___________________________________");
+
+        HashSet<String> hashSet = arrayToHashSet(array);
+        for(String s : hashSet) {
+            System.out.println(s);
+        }
+    }
+
+    public static HashSet<String> arrayToHashSet(String[] strings) {
+        return new HashSet<>(Arrays.asList(strings));
+    }
+}
