1366f019c9efba4323a3a37fb44d5e2899403e89
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:35:49 +0300

    add JavaRushTasks JavaSyntax pro task 0602

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0602/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task06.task0602;
+
+/* 
+Упорядоченная информация
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Я — поэт, зовусь я Цветик.");
+        System.out.println("От меня вам всем приветик.");
+    }
+}
