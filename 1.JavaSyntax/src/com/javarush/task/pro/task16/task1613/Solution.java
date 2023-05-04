f0b91eeb3a8fc28dadf4fab74afd6b10d2e65df6
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:40:38 +0300

    add JavaRushTasks JavaSyntax pro task 1613

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1613/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task16.task1613;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Фильтруем выходные
+*/
+
+public class Solution {
+
+    private static final int FRIDAY = 5;
+    private static final int SATURDAY = 6;
+    private static final int SUNDAY = 7;
+    private static final int WEEKEND_START_FRIDAY_CUT_OFF_HOUR = 22;
+    private static final int WEEKEND_END_SUNDAY_CUT_OFF_HOUR = 23;
+
+    public static void main(String[] args) {
+        List<LocalDateTime> dateTimeList = new ArrayList<>();
+        dateTimeList.add(LocalDateTime.of(2016, 4, 22, 18, 39));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 22, 21, 59));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 22, 22, 0));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 23, 5, 0));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 24, 8, 0));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 24, 22, 59));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 24, 23, 0));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 25, 11, 5));
+
+        for (LocalDateTime dateTime : dateTimeList) {
+            System.out.println(dateTime + ", is weekend - " + isWeekend(dateTime));
+        }
+
+    }
+
+    public static boolean isWeekend(LocalDateTime dateTime) {
+        if(dateTime.getDayOfWeek().equals(DayOfWeek.FRIDAY) && dateTime.getHour() >= WEEKEND_START_FRIDAY_CUT_OFF_HOUR) {
+            return true;
+        }
+
+        if(dateTime.getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
+            return true;
+        }
+
+        if(dateTime.getDayOfWeek().equals(DayOfWeek.SUNDAY) && dateTime.getHour() < WEEKEND_END_SUNDAY_CUT_OFF_HOUR) {
+            return true;
+        }
+
+        return false;
+    }
+}
