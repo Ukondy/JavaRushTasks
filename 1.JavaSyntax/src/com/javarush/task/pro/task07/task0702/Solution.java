a55a7ffa7f29870785ccad4cdd2bc997f405bf6a
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:49:16 +0300

    add JavaRushTasks JavaSyntax pro task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    int a = 2147483647;
+    char f = 127;
+    byte g = 127;
+    long d = 9223372036854775807l;
+    short n = 32767;
+    double j = 	1.7E+308;
+    float l = 3.4E+38f;
+}
