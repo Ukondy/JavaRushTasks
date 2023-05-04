55bd213103844f450245021d5c52729798563fe2
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 22:11:45 +0300

    add JavaRushTasks JavaSyntax pro task 1020

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    public Computer() {
+       SystemUnit systemUnit = new SystemUnit();
+       Mouse mouse = new Mouse();
+       Keyboard keyboard = new Keyboard();
+       Monitor monitor = new Monitor();
+    }
+}
