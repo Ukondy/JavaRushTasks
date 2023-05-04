eea5706434072e90da9d471db45ab1dcdb2c8f4a
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:38:53 +0300

    add JavaRushTasks JavaSyntax pro task 1604

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1604/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task16.task1604;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/* 
+День недели рождения твоего
+*/
+
+public class Solution {
+
+    static Calendar birthDate = new GregorianCalendar(2005, Calendar.SEPTEMBER, 5);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(Calendar calendar) {
+        int numOfDay = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (numOfDay) {
+            case 1:
+                return "Воскресенье";
+            case 2:
+                return "Понедельник";
+            case 3:
+                return "Вторник";
+            case 4:
+                return "Среда";
+            case 5:
+                return "Четверг";
+            case 6:
+                return "Пятница";
+            case 7:
+                return "Суббота";
+        }
+        return null;
+    }
+}
