2721790764531239550c3024be2853aae01e3e80
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:51:14 +0300

    add JavaRushTasks JavaSyntax pro task 1823

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1823/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task18.task1823;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toSet;
+
+/* 
+Из потока данных во множество
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("JavaRush", "CodeGym", "Amigo", "Elly", "Kim", "Risha");
+
+        getFilteredStrings(stringStream).forEach(System.out::println);
+    }
+
+    public static Set<String> getFilteredStrings(Stream<String> stringStream) {
+        Set<String> set = stringStream
+                .filter(e -> e.length() > 6)
+                .collect(Collectors.toSet());
+        return set;
+    }
+}
