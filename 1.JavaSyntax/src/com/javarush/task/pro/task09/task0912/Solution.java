a1961f3021b3a57de1a623c089e7176cf6120a70
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 22:04:04 +0300

    add JavaRushTasks JavaSyntax pro task 0912

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0912/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task09.task0912;
+
+/* 
+Проверка URL-адреса
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] urls = {"https://javarush.ru", "https://google.com", "http://wikipedia.org", "facebook.com", "https://instagram", "codegym.cc"};
+        for (String url : urls) {
+            String protocol = checkProtocol(url);
+            String domain = checkDomain(url);
+
+            System.out.println("У URL-адреса - " + url + ", сетевой протокол - " + protocol + ", домен - " + domain);
+        }
+    }
+
+    public static String checkProtocol(String url) {
+        if(url.startsWith("https")) {
+            return "https";
+        } else if(url.startsWith("http")){
+            return "http";
+        }
+        return "неизвестный";
+    }
+
+    public static String checkDomain(String url) {
+        if (url.endsWith("com")) {
+            return "com";
+        } else if (url.endsWith("net")) {
+            return "net";
+        } else if (url.endsWith("org")) {
+            return "org";
+        } else if (url.endsWith("ru")) {
+            return "ru";
+        }
+        return "неизвестный";
+    }
+}
