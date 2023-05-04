19608ed0d83b84c98aa99308c328cc17776a0f68
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:43:07 +0300

    add JavaRushTasks JavaSyntax pro task 1703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1703/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task17.task1703;
+
+import java.util.ArrayList;
+
+/* 
+Космическая одиссея ч.1
+*/
+
+public class Solution {
+    public static ArrayList<Astronaut> astronauts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        createCrew();
+        printCrewInfo();
+    }
+
+    public static void createCrew(){
+        Human human = new Human();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        astronauts.add(human);
+        astronauts.add(human);
+        astronauts.add(dog);
+        astronauts.add(cat);
+    }
+
+    public static void printCrewInfo() {
+        System.out.println("На борт погружены члены экипажа: ");
+        for (Astronaut astronaut : astronauts) {
+            System.out.println(astronaut.getInfo());
+        }
+    }
+}
