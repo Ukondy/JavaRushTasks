64e237042e4c461e9d8f12c1187e607d261e52db
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:24:10 +0300

    add JavaRushTasks JavaSyntax pro task 1303

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1303/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task13.task1303;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+/* 
+Выводим в консоли элементы множества
+*/
+
+public class Solution {
+
+    public static void print(HashSet<String> words) {
+        Iterator<String> iterator = words.iterator();
+        while(iterator.hasNext()) {
+            String word = iterator.next();
+            System.out.println(word);
+        }
+    }
+
+    public static void main(String[] args) {
+        HashSet<String> words = new HashSet<>(Arrays.asList("Программированию обычно учат на примерах.".split(" ")));
+        print(words);
+    }
+}
