b19fdd19195b71ad43428a62ed9ec974f84f9862
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:31:11 +0300

    add JavaRushTasks JavaSyntax pro task 1411

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1411/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task14.task1411;
+
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystemException;
+import java.nio.file.Files;
+
+/* 
+Распаковка исключений
+*/
+
+public class Solution {
+
+    public static final String FAILED_TO_READ = "Не удалось прочесть файл.";
+    public static final String FAILED_TO_WRITE = "Не удалось записать в файл.";
+
+    public static FileManager fileManager = new FileManager();
+
+    public static void main(String[] args) {
+        try {
+            fileManager.copyFile("book.txt", "book_final_copy.txt");
+            fileManager.copyFile("book_final_copy.txt", "book_last_copy.txt");
+        } catch (RuntimeException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof FileNotFoundException) {
+                FileNotFoundException fnf = (FileNotFoundException) cause;
+                System.out.println(FAILED_TO_READ);
+            } else if (cause instanceof FileSystemException) {
+                FileSystemException fse = (FileSystemException) cause;
+                System.out.println(FAILED_TO_WRITE);
+            }
+        }
+    }
+}
