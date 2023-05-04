be61bda15e7f882c3d867d984a9a15ad9018afd9
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:35:20 +0300

    add JavaRushTasks JavaSyntax pro task 1512

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1512/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task15.task1512;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/* 
+Задом наперед
+*/
+
+public class Solution {
+    public static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    public static PrintStream stream = new PrintStream(outputStream);
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        printSomething(scanner.nextLine());
+        String result = outputStream.toString();
+        outputStream.reset();
+        StringBuilder builder = new StringBuilder(result);
+        String str = builder.reverse().toString();
+        outputStream.writeBytes(str.getBytes());
+        System.out.println(outputStream);
+    }
+
+    public static void printSomething(String str) {
+        stream.print(str);
+    }
+}
