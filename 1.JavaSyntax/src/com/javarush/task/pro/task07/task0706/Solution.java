73f6f32ad470ecd7532f3c786ae9960877067aca
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:50:24 +0300

    add JavaRushTasks JavaSyntax pro task 0706

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0706/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0706;
+
+/* 
+Странное деление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        div(0, 0);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
