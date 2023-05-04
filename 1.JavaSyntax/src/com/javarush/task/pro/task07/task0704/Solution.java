8bb6c60e5fd960cd87d5e22e198262963053a224
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:49:47 +0300

    add JavaRushTasks JavaSyntax pro task 0704

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0704/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0704;
+
+/* 
+Экспонентная запись числа
+*/
+
+public class Solution {
+    double earthDiameter = 1.27420E+4;
+    double lightSpeed = 2.997924580E+8;
+    double uraniumAtomicMass = 2.380289E+2;
+    double averageBeeWeight = 8.5E-3;
+    double javaDeveloperSalary = 1.00000E+4;
+}
