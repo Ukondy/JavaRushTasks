d33fad9ea8365c80550bf6ea19fea9e3dc98095b
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:55:56 +0300

    add JavaRushTasks JavaSyntax pro task 0809

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0809/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task08.task0809;
+
+/* 
+Counter
+
+*/
+public class Solution {
+
+    public static void main(String[] args) throws InterruptedException {
+        printNumbers();
+    }
+
+    public static void printNumbers() throws InterruptedException {
+        System.out.println(1);
+        Thread.sleep(1,300000);
+        System.out.println(2);
+        Thread.sleep(1,300000);
+        System.out.println(3);
+        Thread.sleep(1,300000);
+        System.out.println(4);
+        Thread.sleep(1,300000);
+        System.out.println(5);
+        Thread.sleep(1,300000);
+        System.out.println(6);
+        Thread.sleep(1,300000);
+        System.out.println(7);
+        Thread.sleep(1,300000);
+        System.out.println(8);
+        Thread.sleep(1,300000);
+        System.out.println(9);
+        Thread.sleep(1,300000);
+        System.out.println(10);
+    }
+}
