8d2024b5334486e5a02233566bceecabaa089e38
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:54:50 +0300

    add JavaRushTasks JavaSyntax pro task 0805

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0805/MagicBall.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task08.task0805;
+
+import java.util.Random;
+
+public class MagicBall {
+    private static final String CERTAIN = "Бесспорно";
+    private static final String DEFINITELY = "Определённо да";
+    private static final String MOST_LIKELY = "Вероятнее всего";
+    private static final String OUTLOOK_GOOD = "Хорошие перспективы";
+    private static final String ASK_AGAIN_LATER = "Спроси позже";
+    private static final String TRY_AGAIN = "Попробуй снова";
+    private static final String NO = "Мой ответ — нет";
+    private static final String VERY_DOUBTFUL = "Весьма сомнительно";
+
+    public static String getPrediction() {
+        int a = new Random().nextInt(8);
+        System.out.println(a);
+        switch (a) {
+            case 0:
+                return CERTAIN;
+            case 1:
+                return DEFINITELY;
+            case 2:
+                return MOST_LIKELY;
+            case 3:
+                return OUTLOOK_GOOD;
+            case 4:
+                return ASK_AGAIN_LATER;
+            case 5:
+                return TRY_AGAIN;
+            case 6:
+                return NO;
+            case 7:
+                return VERY_DOUBTFUL;
+            default:
+                return null;
+        }
+    }
+}
