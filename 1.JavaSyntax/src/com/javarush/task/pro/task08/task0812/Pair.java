7fae3bd5883bc92b4187c72374792b6ca48bce48
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:57:23 +0300

    add JavaRushTasks JavaSyntax pro task 0812

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0812/Pair.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task08.task0812;
+
+public class Pair {
+    private int x;
+    private int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("x=%d, y=%d", x, y);
+    }
+
+    public void swap() {
+        int temp = x;
+        x = y;
+        y = temp;
+        //напишите тут ваш код
+    }
+}
