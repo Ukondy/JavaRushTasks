259372352cc57803609fd57381a054acd8a5a9bb
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:48:51 +0300

    add JavaRushTasks JavaSyntax pro task 1812

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1812/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task18.task1812;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/* 
+Рейтинг языков программирования
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<Language> languages = new ArrayList<>();
+        Collections.addAll(languages,
+                new Language("С#", 4.58),
+                new Language("JavaScript", 2.54),
+                new Language("Python", 10.47),
+                new Language("C++", 7.11),
+                new Language("Go", 1.46),
+                new Language("R", 2.37),
+                new Language("Java", 13.48),
+                new Language("C", 15.95),
+                new Language("Swift", 1.38),
+                new Language("PHP", 2.49));
+
+        Stream<Language> languageStream = sortByRanking(languages);
+        languageStream.forEach(System.out::println);
+    }
+
+    public static Stream<Language> sortByRanking(ArrayList<Language> languages) {
+        Stream<Language> stream = languages.stream()
+                .sorted((o1, o2) -> Double.compare(o2.getRanking(), o1.getRanking()));
+        return stream;
+    }
+}
