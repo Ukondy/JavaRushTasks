939e532e3e08fb75c041f59e9781533d6d61cfa5
ваше имяДима <dimapotrebic@gmail.com>
2023-05-05 02:26:36 +0300

    add JavaRushTasks JavaSyntax pro task 1313

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1313/StringsLinkedList.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task13.task1313;
+
+public class StringsLinkedList {
+    private Node first = new Node();
+    private Node last = new Node();
+
+    public StringsLinkedList() {
+        first.next = last;
+        last.prev = first;
+    }
+
+    public void printAll() {
+        Node currentElement = first.next;
+        while ((currentElement) != null) {
+            System.out.println(currentElement.value);
+            currentElement = currentElement.next;
+        }
+    }
+
+    public void add(String value) {
+        Node node = new Node();
+        node.value = value;
+
+        Node lastNode = last.prev;
+        lastNode.next = node;
+        node.prev = lastNode;
+        last.prev = node;
+    }
+
+    public static class Node {
+        private Node prev;
+        private String value;
+        private Node next;
+    }
+}
