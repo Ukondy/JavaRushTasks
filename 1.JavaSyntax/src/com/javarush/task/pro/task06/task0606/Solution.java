2bfa3f2c692023619189dcdbcb207405500280cb
ваше имяДима <dimapotrebic@gmail.com>
2023-05-04 21:43:25 +0300

    add JavaRushTasks JavaSyntax pro task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(String a){
+
+    }
+    public static void universalMethod(String a, String b){
+
+    }
+    public static void universalMethod(int a){
+
+    }
+    public static void universalMethod(int a, int b){
+
+    }
+    public static void universalMethod(long a){
+
+    }
+    public static void universalMethod(long a, long b){
+
+    }
+    public static void universalMethod(boolean a){
+
+    }
+    public static void universalMethod(boolean a, boolean b){
+
+    }
+    public static void universalMethod(double a){
+
+    }
+    public static void universalMethod(double a, double b){
+
+    }
+
+
+    //напишите тут ваш код
+}
