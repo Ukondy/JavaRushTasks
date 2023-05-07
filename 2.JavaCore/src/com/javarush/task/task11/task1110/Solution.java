fdb91ed8df7725944d65c91fe401077cdd038f28
ваше имяДима <dimapotrebic@gmail.com>
2023-05-07 03:01:54 +0300

    add JavaRushTasks JavaCore task 1110

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1110/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task11.task1110;
+
+/* 
+Не забываем инкапсулировать
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
+        public Cat(String name, int age, int weight, int speed) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.speed = speed;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public void setSpeed(int speed) {
+            this.speed = speed;
+        }
+    }
+}
