e0995fa11e3638d3a3bbff8a182cc966b79c6ccf
ваше имяДима <dimapotrebic@gmail.com>
2023-05-12 21:59:51 +0300

    add JavaRushTasks JavaCore task 1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(12);
+        print(new Integer(10));
+    }
+
+    public static void print(Integer a) {
+
+    }
+    public static void print(int a) {
+
+    }
+}
