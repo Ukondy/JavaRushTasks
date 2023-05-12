f38fc45a24de2dcf8561c1a95c0f128a5c4cda09
ваше имяДима <dimapotrebic@gmail.com>
2023-05-13 02:42:34 +0300

    add JavaRushTasks JavaCore task 1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print (int a) {}
+    public static void print (String a) {}
+    public static void print (boolean a) {}
+    public static void print (double a) {}
+    public static void print (byte a) {}
+}
