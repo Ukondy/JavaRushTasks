edcca82ea42e2ef393fb9f50436b05a056aa4299
ваше имяДима <dimapotrebic@gmail.com>
2023-05-12 16:43:17 +0300

    add JavaRushTasks JavaMultithreading task 3608 MVC(1/9)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -0,0 +1,6 @@
+package com.javarush.task.task36.task3608.model;
+
+public interface Model {
+    ModelData getModelData();
+    void loadUsers();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModelData {
+    private List<User> users = new ArrayList<>();
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
