6563431c0e69e06a39361948432dac72492316da
ваше имяДима <dimapotrebic@gmail.com>
2023-05-12 21:59:35 +0300

    add JavaRushTasks JavaCore task 1206

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {
+
+    }
+    public static void print(String a) {
+
+    }
+}
