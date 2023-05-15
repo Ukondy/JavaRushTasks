0588ec63f2b661888b4dee3839d4e31def791570
ваше имяДима <dimapotrebic@gmail.com>
2023-05-15 16:50:16 +0300

    add JavaRushTasks JavaMultithreading task 3608 MVC(3/9)

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -4,15 +4,16 @@ import com.javarush.task.task36.task3608.model.Model;
 import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Controller {
+
     private Model model;
     private UsersView usersView;
 
-    public void setUsersView(UsersView usersView) {
-        this.usersView = usersView;
-    }
-
     public void setModel(Model model) {
         this.model = model;
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
     }
 
     public void onShowAllUsers() {
--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -7,7 +7,7 @@ public class UsersView implements View{
     private Controller controller;
     @Override
     public void refresh(ModelData modelData) {
-        System.out.println("All users: ");
+        System.out.println("All users:");
         modelData.getUsers().forEach(u -> System.out.println("\t" + u));
         System.out.println("===================================================");
     }
