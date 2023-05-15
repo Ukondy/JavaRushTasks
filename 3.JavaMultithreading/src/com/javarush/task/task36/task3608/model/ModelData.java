6c302cca8dbb263a3c7c991660ea50b645065501
ваше имяДима <dimapotrebic@gmail.com>
2023-05-15 17:25:06 +0300

    add JavaRushTasks JavaMultithreading task 3608 MVC(6/9)

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class ModelData {
     private List<User> users = new ArrayList<>();
+    private User activeUser;
+    private boolean displayDeletedUserList;
 
     public List<User> getUsers() {
         return users;
@@ -14,5 +16,21 @@ public class ModelData {
 
     public void setUsers(List<User> users) {
         this.users = users;
+    }
+
+    public User getActiveUser() {
+        return activeUser;
+    }
+
+    public void setActiveUser(User activeUser) {
+        this.activeUser = activeUser;
+    }
+
+    public boolean isDisplayDeletedUserList() {
+        return displayDeletedUserList;
+    }
+
+    public void setDisplayDeletedUserList(boolean displayDeletedUserList) {
+        this.displayDeletedUserList = displayDeletedUserList;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
@@ -3,13 +3,14 @@ package com.javarush.task.task36.task3608.view;
 import com.javarush.task.task36.task3608.controller.Controller;
 import com.javarush.task.task36.task3608.model.ModelData;
 
-public class UsersView implements View{
+public class EditUserView implements View {
+    private ModelData modelData = new ModelData();
     private Controller controller;
 
     @Override
     public void refresh(ModelData modelData) {
-        System.out.println("All " + (modelData.isDisplayDeletedUserList() ? "deleted " : "") + "users:");
-        modelData.getUsers().forEach(u -> System.out.println("\t" + u));
+        System.out.println("User to be edited:");
+        System.out.println("\t" + modelData.getActiveUser());
         System.out.println("===================================================");
     }
 
@@ -17,12 +18,4 @@ public class UsersView implements View{
     public void setController(Controller controller) {
         this.controller = controller;
     }
-
-    public void fireEventShowAllUsers() {
-        controller.onShowAllUsers();
-    }
-    public void fireEventShowDeletedUsers() {
-        controller.onShowAllDeletedUsers();
-    }
-
 }
