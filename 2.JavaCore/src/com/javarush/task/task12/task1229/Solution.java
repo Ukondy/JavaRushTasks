fadfb9a84eb015056fc01c12b48568acf2a0d050
ваше имяДима <dimapotrebic@gmail.com>
2023-05-16 21:18:13 +0300

    add JavaRushTasks JavaCore task 1229

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1229/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task12.task1229;
+
+/* 
+Родитель класса CTO
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static class MyClass {
+        public void workHard() {
+
+        }
+    }
+    public static class CTO extends MyClass implements Businessman {
+
+    }
+}
