656b919e6e6f9f0a1f4b7ca6f3cdd89556f98f13
ваше имяДима <dimapotrebic@gmail.com>
2023-05-25 18:45:40 +0300

    add JavaRushTasks JavaMultithreading task 2909 Refactoring(3/15)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Human {
+public class Human implements Alive {
     private List<Human> children = new ArrayList<>();
     public static int nextId = 0;
 
     private int id;
     protected int age;
     protected String name;
-    protected int course;
 
     protected int[] size;
 
@@ -52,10 +51,6 @@ public class Human {
         this.name = name;
     }
 
-    public int getCourse() {
-        return course;
-    }
-
     public int getId() {
         return id;
     }
@@ -77,6 +72,11 @@ public class Human {
     }
     public void removeChild(Human human) {
         children.remove(human);
+    }
+
+    @Override
+    public void live() {
+
     }
 }
 
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -9,6 +9,8 @@ public class Student extends Human {
     private String university;
     private Date beginningOfSession;
     private Date endOfSession;
+    private int course;
+
 
     public Student(String name, int age, double averageGrade) {
         super(name, age);
@@ -63,5 +65,9 @@ public class Student extends Human {
 
     public double getAverageGrade() {
         return averageGrade;
+    }
+
+    public int getCourse() {
+        return course;
     }
 }
