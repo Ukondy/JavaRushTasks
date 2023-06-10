ce6d75b18660f78b4d77af3eac9bec699ab807ac
ваше имяДима <dimapotrebic@gmail.com>
2023-06-10 16:32:16 +0300

    add JavaRushTasks JavaMultithreading task 3008 Chat(7/22)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,12 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("get server's port");
         int port = ConsoleHelper.readInt();
@@ -19,6 +23,16 @@ public class Server {
             }
         } catch (IOException e) {
             ConsoleHelper.writeMessage("Exception");
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for(Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("sending message was failed: " + connection.getRemoteSocketAddress());
+            }
         }
     }
 
