2f20b2c645b35fbbcfc4c41c761fd85239fe4ff4
ваше имяДима <dimapotrebic@gmail.com>
2023-06-10 15:52:40 +0300

    add JavaRushTasks JavaMultithreading task 3008 Chat(2/22)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,35 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String string = "";
+        try {
+            string = reader.readLine();
+        } catch (IOException e) {
+            writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            string = readString();
+        }
+        return string;
+    }
+
+    public static int readInt() {
+        int number = 0;
+        try {
+            number = Integer.parseInt(readString());
+        } catch(NumberFormatException e) {
+            writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            number = readInt();
+        }
+        return number;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -4,4 +4,5 @@ public class Server {
     public static void main(String[] args) {
 
     }
+
 }
