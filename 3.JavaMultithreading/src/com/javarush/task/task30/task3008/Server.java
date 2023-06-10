79d7bcdaed017372ec68912a333721ff295e91f7
ваше имяДима <dimapotrebic@gmail.com>
2023-06-10 16:26:56 +0300

    add JavaRushTasks JavaMultithreading task 3008 Chat(6/22)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,37 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
+        ConsoleHelper.writeMessage("get server's port");
+        int port = ConsoleHelper.readInt();
 
+        try(ServerSocket serverSocket = new ServerSocket(port);
+        ) {
+            ConsoleHelper.writeMessage("server ready");
+            while(true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Exception");
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+        }
     }
 }
