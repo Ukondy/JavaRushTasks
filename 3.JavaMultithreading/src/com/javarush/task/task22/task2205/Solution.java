514717aa752969208b40ef91d941c29d8c6079c5
ваше имяДима <dimapotrebic@gmail.com>
2023-06-11 16:49:42 +0300

    add JavaRushTasks JavaMultithreading task 3008 Chat(9/22)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2205/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task22.task2205;
+
+/* 
+МНЕ нравится курс JavaRush
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(String.format(getFormattedString(), "JavaRush", "курс", "мне", "нравится"));
+        //должен быть вывод
+        //"МНЕ нравится курс JavaRush"
+    }
+
+    public static String getFormattedString() {
+        return "%3$2S %4$2s %2$2s %1$2s";
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -59,6 +59,14 @@ public class Server {
             }
         }
 
+        private void notifyUsers(Connection connection, String userName) throws IOException {
+            for(String name : connectionMap.keySet()) {
+                if(!name.equals(userName)) {
+                    connection.send(new Message(MessageType.USER_ADDED, name));
+                }
+            }
+        }
+
         @Override
         public void run() {
             super.run();
