63e40424c3a7fb80031262f32ffd882d541fab39
ваше имяДима <dimapotrebic@gmail.com>
2023-06-12 17:45:44 +0300

    add JavaRushTasks JavaMultithreading task 3008 Chat(13/22)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,10 +1,47 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("get server address");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("get server port");
+        return ConsoleHelper.readInt();
+    }
+    protected String getUserName () {
+        ConsoleHelper.writeMessage("get user name");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("fail in sending message");
+            clientConnected = false;
+        }
+    }
 
     public class SocketThread extends Thread {
 
