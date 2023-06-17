cf02ae400157778e9f99bd664889179a21253cf0
ваше имяДима <dimapotrebic@gmail.com>
2023-06-17 19:45:03 +0300

    add JavaRushTasks JavaMultithreading task 3008 Chat(18/22)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task30.task3008.client;
+
+public class BotClient extends Client {
+
+    public static void main(String[] args) {
+        BotClient botClient = new BotClient();
+        botClient.run();
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new BotSocketThread();
+    }
+
+    @Override
+    protected boolean shouldSendTextFromConsole() {
+        return false;
+    }
+
+    @Override
+    protected String getUserName() {
+        return "date_bot_" + (int) (Math.random() * 100);
+    }
+
+    public class BotSocketThread extends SocketThread {
+
+    }
+}
