37ed6b884f73d8246ca387bd00a75ae4ceb49416
ваше имяДима <dimapotrebic@gmail.com>
2023-06-26 16:49:56 +0300

    add JavaRushTasks JavaMultithreading task 3008 Archiver(2/18)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,7 +14,18 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        try(ZipOutputStream outputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+            InputStream inputStream = Files.newInputStream(source);
+            ) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            outputStream.putNextEntry(zipEntry);
 
+            byte[] buffer = new byte[8*1024];
+            while(inputStream.available() > 0) {
+                int size = inputStream.read(buffer);
+                outputStream.write(buffer, 0, size);
+            }
+        }
     }
 
 
