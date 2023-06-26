a08423d881a3742a543e9b3184b2a78a5b938745
ваше имяДима <dimapotrebic@gmail.com>
2023-06-26 17:23:03 +0300

    add JavaRushTasks JavaMultithreading task 3008 Archiver(6/18)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task31.task3110;
+
+import com.javarush.task.task31.task3110.command.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandExecutor {
+    private static final Map<Operation, Command> ALL_KNOWN_COMMANDS_MAP = new HashMap<>();
+
+    private CommandExecutor() {}
+
+    static {
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.ADD, new ZipAddCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.CONTENT, new ZipContentCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.CREATE, new ZipCreateCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.EXTRACT, new ZipExtractCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.REMOVE, new ZipRemoveCommand());
+        ALL_KNOWN_COMMANDS_MAP.put(Operation.EXIT, new ExitCommand());
+    }
+
+    public static void execute(Operation operation) throws Exception {
+        ALL_KNOWN_COMMANDS_MAP.get(operation).execute();
+    }
+
+}
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -3,6 +3,6 @@ package com.javarush.task.task31.task3110.command;
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        
     }
 }
