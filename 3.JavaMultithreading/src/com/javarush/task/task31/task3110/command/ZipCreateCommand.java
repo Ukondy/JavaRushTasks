da440286eeac8cafaf13a8715da88e6caf0ed154
ваше имяДима <dimapotrebic@gmail.com>
2023-07-03 16:14:54 +0300

    add JavaRushTasks JavaMultithreading task 3008 Archiver(10/18)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -1,8 +1,28 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
 
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла");
+            Path path = Paths.get(ConsoleHelper.readString());
+
+            zipFileManager.createZip(path);
+
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch(PathIsNotFoundException ex) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
