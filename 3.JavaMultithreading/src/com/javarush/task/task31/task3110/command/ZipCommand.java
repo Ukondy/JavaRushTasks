019ba199ca543e9003e21696ab36b98bc23a37aa
ваше имяДима <dimapotrebic@gmail.com>
2023-07-03 16:44:02 +0300

    add JavaRushTasks JavaMultithreading task 3008 Archiver(11/18)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,5 +1,18 @@
 package com.javarush.task.task31.task3110.command;
 
-public abstract class ZipCommand implements Command {
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
 
+import java.nio.file.Paths;
+
+public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception {
+        String path;
+
+        ConsoleHelper.writeMessage("Введите имя файла");
+        path = ConsoleHelper.readString();
+
+        return new ZipFileManager(Paths.get(path));
+    }
 }
+
