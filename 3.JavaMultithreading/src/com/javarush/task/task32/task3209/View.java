0b4a8a768bc695b4bffbadb64e7a9141915cc2bd
ваше имяДима <dimapotrebic@gmail.com>
2023-07-17 16:54:16 +0300

    add JavaRushTasks JavaMultithreading task 3209 HTML Editor(3/24)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
 
     public void init() {
 
