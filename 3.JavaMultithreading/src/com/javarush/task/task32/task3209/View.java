347256a6edcd570d7b50e9914c00fd2890077966
ваше имяДима <dimapotrebic@gmail.com>
2023-07-17 17:35:26 +0300

    add JavaRushTasks JavaMultithreading task 3209 HTML Editor(6/24)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -24,7 +26,12 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
-
+        htmlTextPane.setContentType("text/html");
+        tabbedPane.add("HTML", new JScrollPane(htmlTextPane));
+        tabbedPane.add("Текст", new JScrollPane(plainTextPane));
+        tabbedPane.setPreferredSize(new Dimension(100, 100));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
 
     public void initGui() {
