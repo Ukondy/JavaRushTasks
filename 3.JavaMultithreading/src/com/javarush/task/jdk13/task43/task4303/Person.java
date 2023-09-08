ba410bd310157cc3aadc2e3360ca43f2ef14870d
ваше имяДима <dimapotrebic@gmail.com>
2023-09-08 16:49:18 +0300

    add JavaRushTasks JavaMultithreading jdk13 task 4303

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task43/task4303/Person.java
@@ -0,0 +1,37 @@
+package com.javarush.task.jdk13.task43.task4303;
+
+/* 
+Нам шаблоны не нужны
+*/
+
+import org.apache.commons.lang3.builder.StandardToStringStyle;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Person {
+    private String name;
+    private int age;
+    private int weight;
+    private int height;
+    private transient String sex;
+
+    public Person(String name, int age, int weight, int height, String sex) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        StandardToStringStyle style = new StandardToStringStyle();
+        style.setUseClassName(false);
+        style.setUseIdentityHashCode(false);
+        style.setContentStart("This persons ");
+        style.setFieldNameValueSeparator(" is ");
+        style.setFieldSeparator("; ");
+        style.setContentEnd(".");
+
+        return ToStringBuilder.reflectionToString(this, style);
+    }
+}
