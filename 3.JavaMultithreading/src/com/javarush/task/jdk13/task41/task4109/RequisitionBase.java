b5d82f219b38ad175deae009d391b58c5baea8bc
ваше имяДима <dimapotrebic@gmail.com>
2023-09-18 16:50:17 +0300

    add JavaRushTasks JavaMultithreading jdk13 task 4109

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4109/RequisitionBase.java
@@ -0,0 +1,25 @@
+package com.javarush.task.jdk13.task41.task4109;
+
+public abstract class RequisitionBase extends AbstractEntity {
+    public User user;
+    public String name;
+    public String mail;
+    public String utmMark;
+    public String webinarType;
+    public String promoCode;
+
+    public RequisitionBase() {
+
+    }
+
+    public RequisitionBase(RequisitionBase requisitionBase) {
+        super(requisitionBase);
+        this.user = (User) requisitionBase.user.clone();
+        this.name = requisitionBase.name;
+        this.mail = requisitionBase.mail;
+        this.utmMark = requisitionBase.utmMark;
+        this.webinarType = requisitionBase.webinarType;
+        this.promoCode = requisitionBase.promoCode;
+    }
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4109/User.java
@@ -0,0 +1,46 @@
+package com.javarush.task.jdk13.task41.task4109;
+
+public class User extends AbstractEntity {
+    public String socialKey;
+    public String secretKey;
+    public String displayName;
+    public String pictureUrl;
+    public String sessionId;
+    public String referenceKey;
+    public int level;
+    public int lesson;
+    public int darkMatter;
+    public int rating;
+    public String email;
+    public String country;
+    public String city;
+    public String statusMessage;
+
+
+
+    public User() {
+    }
+
+    public User(User user) {
+        super(user);
+        this.socialKey = user.socialKey;
+        this.secretKey = user.secretKey;
+        this.displayName = user.displayName;
+        this.pictureUrl = user.pictureUrl;
+        this.sessionId = user.sessionId;
+        this.referenceKey = user.referenceKey;
+        this.level = user.level;
+        this.lesson = user.lesson;
+        this.darkMatter = user.darkMatter;
+        this.rating = user.rating;
+        this.email = user.email;
+        this.country = user.country;
+        this.city = user.city;
+        this.statusMessage = user.statusMessage;
+    }
+
+    @Override
+    public AbstractEntity clone() {
+        return new User(this);
+    }
+}
