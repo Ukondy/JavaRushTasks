8006b5a6d1461241c6d943b6670a2dbde40b5aa1
ваше имяДима <dimapotrebic@gmail.com>
2023-09-22 17:50:06 +0300

    add JavaRushTasks JavaMultithreading task 2801

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2801/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task28.task2801;
+
+/* 
+Осваиваем switch
+*/
+
+public class Solution<number> {
+    public static enum E1 {A, B, C, Y}
+
+    public static enum E2 {D, E, F}
+
+    public static enum E3 {D, E, F}
+
+    public static void main(String[] args) {
+        Solution.switchTest(E1.C);
+        Solution.switchTest(E3.D);
+        Solution.switchTest(E2.D);
+        /* output
+        it's E1.C
+        undefined
+        it's E2.D
+         */
+    }
+
+    public static void switchTest(Enum<?> obj) {
+        String className = obj.getDeclaringClass().getSimpleName();
+        switch(className) {
+            case "E1":
+            case "E2":
+                System.out.println("it's " + obj.getDeclaringClass().getSimpleName() + "." + obj.name());
+                break;
+            default:
+                System.out.println("undefined");
+        }
+    }
+}
