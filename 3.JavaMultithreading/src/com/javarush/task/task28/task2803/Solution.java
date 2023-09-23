70db9af70bdbea1a83b6a8390cfd836728bd9c97
ваше имяДима <dimapotrebic@gmail.com>
2023-09-23 17:50:58 +0300

    add JavaRushTasks JavaMultithreading task 2803

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2803/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task28.task2803;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/* 
+ThreadLocalRandom
+*/
+
+public class Solution {
+    public static int getRandomIntegerBetweenNumbers(int from, int to) {
+        return ThreadLocalRandom.current().nextInt(from, to);
+    }
+
+    public static double getRandomDouble() {
+        return ThreadLocalRandom.current().nextDouble();
+    }
+
+    public static long getRandomLongBetween0AndN(long n) {
+        return ThreadLocalRandom.current().nextLong(n);
+    }
+
+    public static void main(String[] args) {
+    }
+}
