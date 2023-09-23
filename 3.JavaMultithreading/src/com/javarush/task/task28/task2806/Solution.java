a42c8a162e4868bd3e67c1b2bd1ed3db5c18dfa6
ваше имяДима <dimapotrebic@gmail.com>
2023-09-23 18:25:34 +0300

    add JavaRushTasks JavaMultithreading task 2806

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2806/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task28.task2806;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+/* 
+Знакомство с Executors
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executors = Executors.newFixedThreadPool(5);
+        IntStream.range(1,11).mapToObj(i -> (Runnable) () -> doExpensiveOperation(i)).forEach(executors::submit);
+        executors.shutdown();
+        executors.awaitTermination(5, TimeUnit.SECONDS);
+
+        /* output example
+pool-1-thread-2, localId=2
+pool-1-thread-1, localId=1
+pool-1-thread-3, localId=3
+pool-1-thread-1, localId=7
+pool-1-thread-1, localId=9
+pool-1-thread-4, localId=4
+pool-1-thread-5, localId=5
+pool-1-thread-2, localId=6
+pool-1-thread-1, localId=10
+pool-1-thread-3, localId=8
+         */
+    }
+
+    private static void doExpensiveOperation(int localId) {
+        System.out.println(Thread.currentThread().getName() + ", localId=" + localId);
+    }
+}
