c53bc72572b51df2470cfe2b1b95a49ff81e07a2
Ukondy <dimapotrebic@gmail.com>
2023-09-25 12:30:11 +0300

    add JavaRushTasks JavaMultithreading task 2813

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2813/FactorialCalculator.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task28.task2813;
+
+import java.util.concurrent.Callable;
+
+public class FactorialCalculator implements Callable {
+    private final int number;
+
+    public FactorialCalculator(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public Long call() {
+        try {
+            return factorial(number);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public long factorial(int number) throws InterruptedException {
+        if (number < 0) {
+            throw new IllegalArgumentException("Number must be greater than zero");
+        }
+        long result = 1;
+        while (number > 1) {
+            Thread.sleep(1);
+            result = result * number;
+            number--;
+        }
+        return result;
+    }
+}
