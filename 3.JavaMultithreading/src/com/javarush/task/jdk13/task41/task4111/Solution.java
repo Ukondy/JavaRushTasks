e820b1f61e910c1190fc110179e83d20d69a71b2
Ukondy <ukondy.work@gmail.com>
2023-10-10 23:34:54 +0300

    add JavaRushTasks JavaMultithreading jdk13 task 4111

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4111/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.jdk13.task41.task4111;
+
+/* 
+Ленивый зам
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Entity entity = new LazyInitializer(10001);
+        System.out.println(entity.getId());
+        System.out.println(entity.find("John"));
+    }
+}
